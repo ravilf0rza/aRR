56ccabc33c60bba76dcaaad0c5d8a1509a07d1a9
ravilf0rza <forzanarchyy@gmail.com>
2024-05-28 04:32:13 +0400

    added new files

---
+++ main.js
@@ -0,0 +1,29 @@
+// const numbers = [7, 5, 2, 8, 1, 4, 88, 99];
+// const minElement = Math.min(...numbers);
+// console.log('Minimum Element:', minElement);
+
+// const numbers = [7, 5, 2, 8, 1, 4, 88, 99];
+//  numbers.sort();
+// alert(numbers);
+
+// const arrey = [1,2,3,13,31,14,88,999]
+// for (let i = 0; i < arrey.length; i++) {   
+// for (let j = i + 1; j < arrey.length; j++) {
+//     if (arrey[j] < arrey[i])  {
+//     const temp = arrey[j]
+//     arrey[j] = arrey[i]
+//     arrey[i] = temp
+//     }
+// }
+
+// }
+// console.log(arrey);
+
+
+
+
+
+
+
+
+
